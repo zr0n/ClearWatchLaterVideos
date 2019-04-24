7b596f73526ceebe7d8c1bdd2cb16489659e396b
Luiz Fernando <fernandoaff1@gmail.com>
2019-04-24 16:08:46 -0300

    Add delayed clicks

--- remove-from-watch.js
+++ remove-from-watch.js
@@ -2,19 +2,23 @@
 /**
 https://www.reddit.com/r/youtube/comments/2jfdzf/how_to_remove_all_videos_from_watch_later/
 */
-
+const RemoveVideo = function(interval){
+	var list = document.getElementsByClassName('pl-video-edit-remove');
+	if(list.length)
+		list[0].click();
+	if(list.length > 1)
+		setTimeout(() => {RemoveVideo(interval)}, interval)
+}
 
 const RemoveAllVideos = function () {
 	console.log("Removing all videos")
-	var el = document.getElementsByClassName('pl-video-edit-remove');
-	for (var i=0;i<el.length; i++) {
-		el[i].click();
-
-	}
+	
+	RemoveVideo(500);
 }
+
 browser.runtime.onMessage.addListener(handleMessage)
 function handleMessage(request, sender, sendResponse){
-	if(request.deleteAllVideos)
+	if(request.deleteAllVideos && confirm("Do you want to delete all videos from your playlist?"))
 		RemoveAllVideos();
 }
 
